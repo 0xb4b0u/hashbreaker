0833d9fdfd317f83fb8cf0fbb9a4911bbd975c88
Babou-Dev <baptiste.oger@icloud.com>
2023-12-26 23:18:40 +0100

    The calculate_hash function is now able to handle errors !

--- lib/hashlib.c
+++ lib/hashlib.c
@@ -3,13 +3,38 @@
 char* calculate_hash(char* str) {
     EVP_MD_CTX *tmp_mdctx;
     tmp_mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit(tmp_mdctx, EVP_md5());
-    EVP_DigestUpdate(tmp_mdctx, str, strlen(str));
+    if (tmp_mdctx == NULL) {
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    if (EVP_DigestInit(tmp_mdctx, EVP_md5()) != 1) {
+        ERR_print_errors_fp(stderr);
+        EVP_MD_CTX_free(tmp_mdctx);
+        return NULL;
+    }
+
+    if (EVP_DigestUpdate(tmp_mdctx, str, strlen(str)) != 1) {
+        ERR_print_errors_fp(stderr);
+        EVP_MD_CTX_free(tmp_mdctx);
+        return NULL;
+    }
     unsigned char md[EVP_MAX_MD_SIZE];
     unsigned int md_len;
-    EVP_DigestFinal(tmp_mdctx, md, &md_len);
+
+    if (EVP_DigestFinal(tmp_mdctx, md, &md_len) != 1) {
+        ERR_print_errors_fp(stderr);
+        EVP_MD_CTX_free(tmp_mdctx);
+        return NULL;
+    }
+
     EVP_MD_CTX_free(tmp_mdctx);
+
     char* hash = malloc(sizeof(char) * (md_len * 2 + 1));
+    if (hash == NULL) {
+        perror("malloc failed");
+        return NULL;
+    }
     for (int i = 0; i < md_len; i++) {
         sprintf(hash + i * 2, "%02x", md[i]);
     }
